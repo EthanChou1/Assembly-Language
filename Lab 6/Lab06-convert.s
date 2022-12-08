0809755eca5a6f0bb1d7b2e2b7473bff2bf69c51
Ethan Chou <89790344+EthanChou1@users.noreply.github.com>
2022-12-07 21:40:41 -0600

    Add files via upload

--- Lab 6/Lab06-convert.s
+++ Lab 6/Lab06-convert.s
@@ -7,6 +7,10 @@
     .align
 int2asc:
 // Your assembly code goes here
+	cmp r0, #0x0A
+	ite lo
+	addlo r0, #0x30
+	addhs r0, #0x37
 	bx	lr
 
 	.global		asc2int
@@ -14,5 +18,9 @@ int2asc:
     .align
 asc2int:
 // Your assembly code goes here
+	cmp r0, #0x3A
+	ite lo
+	sublo r0, #0x30
+	subhs r0, #0x37
 	bx	lr
 	.end
